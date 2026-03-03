961deca8c4a81d8db3112a1c3b565bfe33ccfdc3
Mahone <22247537@qq.com>
2026-03-03 14:08:02 +0800

    chore(db): update KMatrix official website URL in SQL scripts
    在 SQL 部署脚本和基线迁移文件中更新 KMatrix 官网地址为 http://KMatrix.kykms.cn

--- ruoyi-admin/src/main/resources/db/migration/V1.0.0__baseline.sql
+++ ruoyi-admin/src/main/resources/db/migration/V1.0.0__baseline.sql
@@ -2948,7 +2948,7 @@ INSERT INTO km_node_connection_rule (rule_id, source_node_type, target_node_type
 insert into sys_menu values('1', '系统管理', '0', '18', 'system',           null, '', '1', '0', 'M', '0', '0', '', 'system',   103, 1, now(), null, null, '系统管理目录');
 insert into sys_menu values('2', '系统监控', '0', '3', 'monitor',          null, '', '1', '0', 'M', '0', '0', '', 'monitor',  103, 1, now(), null, null, '系统监控目录');
 insert into sys_menu values('3', '系统工具', '0', '4', 'tool',             null, '', '1', '0', 'M', '0', '0', '', 'tool',     103, 1, now(), null, null, '系统工具目录');
-insert into sys_menu values('4', 'KMatrix官网', '0', '999', 'http://www.kyxms.cn', null, '', '0', '0', 'M', '0', '0', '', 'guide',    103, 1, now(), null, null, 'KMatrix官网地址');
+insert into sys_menu values('4', 'KMatrix官网', '0', '999', 'http://KMatrix.kykms.cn', null, '', '0', '0', 'M', '0', '0', '', 'guide',    103, 1, now(), null, null, 'KMatrix官网地址');
 insert into sys_menu VALUES('5', '测试菜单', '0', '5', 'demo',             null, '', '1', '0', 'M', '0', '1', null, 'star',       103, 1, now(), null, null, '测试菜单');
 -- 二级菜单
 insert into sys_menu values('100',  '用户管理',     '1',   '1', 'user',             'system/user/index',            '', '1', '0', 'C', '0', '0', 'system:user:list',            'user',          103, 1, now(), null, null, '用户管理菜单');
